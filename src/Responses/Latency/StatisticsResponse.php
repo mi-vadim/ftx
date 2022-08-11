a2d8ff3d2908ea55277d0ec64e139ae36b88fe95
mi_vadim <vadim.milevskiy@fabros-team.com>
2022-08-11 22:59:08 +0300

    Added response files for latency request

---
+++ src/Responses/Latency/StatisticsResponse.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace FTX\Responses\Latency;
+
+use FTX\Responses\AbstractResponser;
+
+class StatisticsResponse extends AbstractResponser
+{
+    private function __construct(
+        public readonly bool $bursty,
+        public readonly float $p50,
+        public readonly int $requestCount,
+    ){}
+
+    public static function fromArray(array $data): static
+    {
+        return new self(
+            bursty: $data['bursty'],
+            p50: (float) $data['p50'],
+            requestCount: $data['requestCount'],
+        );
+    }
+}
