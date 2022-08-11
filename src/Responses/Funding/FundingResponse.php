5e767b085be556955ad2f319bc57e7095e73019d
mi_vadim <vadim.milevskiy@fabros-team.com>
2022-08-11 22:33:27 +0300

    Added response files for funding request

---
+++ src/Responses/Funding/FundingResponse.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace FTX\Responses\Funding;
+
+use FTX\Responses\AbstractResponser;
+
+class FundingResponse extends AbstractResponser
+{
+    private function __construct(
+        public readonly string $future,
+        public readonly int $id,
+        public readonly float $payment,
+        public readonly string $time
+    ){}
+
+    public static function fromArray(array $data): static
+    {
+        return new self(
+            future: $data['future'],
+            id: $data['id'],
+            payment: $data['payment'],
+            time: $data['time']
+        );
+    }
+}
